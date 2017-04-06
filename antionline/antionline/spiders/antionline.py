f2d3ecd8a96be4134ca44af44e25c06bae8754fc
jiexu <jiexu@pptv.com>
2017-04-06 19:50:50 +0800

    update antionline

--- antionline/antionline/spiders/antionline.py
+++ antionline/antionline/spiders/antionline.py
@@ -71,6 +71,28 @@ class AntionlineSpider(scrapy.Spider):
         infos = response.xpath("//span[@class='postdate old']/span[@class='date']/text()").extract()
         if len(infos):
             bitem['art_pub_time'] = infos[0].strip()
+        
+        if not bitem['art_content']:        
+            for info in response.xpath("//blockquote[starts-with(@class, 'postcontent restore')]/text()").extract():
+                bitem['art_content'] += info.strip()
+
+        infos = response.xpath("//div[@class='username_container']")
+        if infos:
+            names = infos.xpath("span/text()").extract()
+            if len(names) > 0:
+                bitem['art_author'] = names[0]
+            
+            if not bitem['art_author']:
+                names = infos.xpath("div/a/strong/text()").extract()
+                if len(names) > 0:
+                    bitem['art_author'] = names[0]
+        
+        infos = response.xpath(".//*[@id='postpagestats_above']/text()").extract()
+        if len(infos) > 0:
+            s = infos[0].strip()
+            index_1 = s.rfind(' ')
+            if index_1 != -1:
+                bitem['art_read'] = s[index_1:].strip()
 
         if not bitem['art_content']:
             print "content null {}".format(response.url)
