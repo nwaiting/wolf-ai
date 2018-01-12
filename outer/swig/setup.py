e99abd54eaec6d9e31dac62852a86ead5a232a39
xujie <jiexu@pptv.com>
2018-01-12 18:07:42 +0800

    update outer/swig/setup.py

--- outer/swig/setup.py
+++ outer/swig/setup.py
@@ -10,3 +10,15 @@ setup (name = 'example',
             description = """Simple swig example from docs""",
             ext_modules = [example_module],
             py_modules = ["example"], )
+
+"""
+foo_module = Extension('_foo',
+                        sources=['foo.i' , 'foo.cpp'],
+                        swig_opts=['-c++'],
+                        library_dirs=['/usr/lib64'],
+                        libraries=['ex'],
+                        include_dirs = ['/usr/include'],
+                        extra_compile_args = ['-DNDEBUG', '-DUNIX', '-D__UNIX',  '-m64', '-fPIC', '-O2', '-w', '-fmessage-length=0'])
+'-Xlinker -export-dynamic'
+
+"""
