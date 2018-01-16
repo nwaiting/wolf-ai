f37d495615edd8219a81367f32190aaceaa9f582
abcd1f2 <798990255@qq.com>
2018-01-16 23:38:56 +0800

    update outer/CoRelationWithWordType/parse_string.py

--- outer/CoRelationWithWordType/parse_string.py
+++ outer/CoRelationWithWordType/parse_string.py
@@ -1,8 +1,21 @@
 #coding=utf-8
 
-def main():
-    pass
+def parse_str(input, output, filter_list):
+    with open(input_file, 'rb') as finput:
+        with open(out_file, 'wb') as foutput:
+            for line in finput.readlines():
+                line = line.strip().decode()
+                res = line.split(';')
+                write_line = ''
+                for item in res:
+                    dd = [item for i in filter_list if item.endswith(i)]
+                    if dd:
+                        write_line += item + ';'
+                if write_line:
+                    write_line += '\n'
+                    foutput.write(write_line.encode())
 
-if __name__ == '__main__':
-    input_file = 'outer'
-    main()
+input_file = 'outer/CoRelationWithWordType/CoRelationWithWordType.txt'
+out_file = 'outer/CoRelationWithWordType/CoRelationWithWordType.result'
+filters = ['n_jb','n_zz','n_zd','n_qg']
+parse_str(input_file, out_file, filters)
