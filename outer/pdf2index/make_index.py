9d47b8d902d5d9d8f88e625e441169d25f0b0117
abcd1f2 <798990255@qq.com>
2018-01-17 00:03:27 +0800

    update outer/pdf2index/make_index.py

--- outer/pdf2index/make_index.py
+++ outer/pdf2index/make_index.py
@@ -1,7 +1,14 @@
 #coding=utf-8
 
-def main():
-    pass
+import re
+
+def parse_txt(input, output):
+    with open(input_file, 'rb') as finput:
+        with open(output_file, 'wb') as foutput:
+            for line in finput.readlines():
+                if line.
 
 if __name__ == '__main__':
-    main()
+    input_file = 'outer/pdf2index/input_data'
+    output_file = 'outer/pdf2index/output_data'
+    parse_txt(input_file, output_file)
