eb7249a092cee14239ef31404cd81a13ba8e87d0
xujie <jiexu@pptv.com>
2018-01-18 20:23:06 +0800

    update outer/swig

--- outer/swig/pplive_gen/pplive_key_generate.c
+++ outer/swig/pplive_gen/pplive_key_generate.c
@@ -7,6 +7,11 @@ void getKey(char* pkey , time_t t);
 #define EXPORT
 #endif
 
+/*
+    写程序编译成dll文件
+    这个libspam.dll就是我们需要的动态库，将它改名为spam.pyd(python中的pyd其实就是dll)，复制到你的python路径/libs目录下
+*/
+
 static PyObject*  generate_old_key(PyObject* self, PyObject* args)
 {
   int t, result;
@@ -17,10 +22,26 @@ static PyObject*  generate_old_key(PyObject* self, PyObject* args)
 
   getKey( key , t );
 
+  /*
+    s = char *
+    i = int
+    l = long int
+    h = short int
+    c = char
+    f = float
+    d = double
+    O = PyObject *
+    (items) = A tuple
+    |items = Optional arguments
+    Py_BuildValue和PyArg_ParseTuple功能相反，它会将C的值转换成Python的值
+  */
   return Py_BuildValue("s", key);
 }
 
-
+/*
+    PyMethodDef moduleMethods数组定义了需要导出到Python中的名字，函数指针，参数类型，描述信息。注意第三个参数，标志了函数的参数类型
+    METH_VARARGS代表的就是我们写Python时的*args,而METH_KEYWORDS就是Python中的**kwargs,所谓的字典字典变量。描述信息就在Python中就是DocString了
+*/
 static PyMethodDef moduleMethods[] =
 {
   {"generateOldKey", generate_old_key, METH_VARARGS, "generate pplive old key"},
@@ -28,9 +49,12 @@ static PyMethodDef moduleMethods[] =
 };
 
 
+/*
+模块名：
+    最后就需要初始化该模块了，注意名字必须是initXXX，其中XXX就是我们所说的模块名。也就是说我们重命名的pyd文件名，initXXX和Py_InitModule(XXX)三者必须一致
+*/
 EXPORT void initpplive_key_generate()
 {
   PyObject* m;
   m = Py_InitModule("pplive_key_generate", moduleMethods);
-  
 }
--- outer/swig/readme.txt
+++ outer/swig/readme.txt
@@ -7,3 +7,12 @@ Python调用c++两种方法：
 2、自定义
 	a、自定义Python的接口封装接口进行封装和初始化
 	b、使用setup.py进行编译和安装
+
+c/c++写Python扩展：
+	https://www.ibm.com/developerworks/cn/linux/l-pythc/	用C语言扩展Python的功能
+	https://docs.microsoft.com/zh-cn/visualstudio/python/cpp-and-python		创建适用于 Python 的 C++ 扩展
+	http://zqpythonic.qiniucdn.com/data/20060106095504/index.html 	Python 与 C++ 的交互编程
+	http://blog.csdn.net/yueguanghaidao/article/details/11538433	Python之美[从菜鸟到高手]--一步一步动手给Python写扩展(爱之初体验)
+	http://usyiyi.cn/translate/python_352/extending/extending.html	Extending Python with C or C++
+	http://blog.guoyb.com/2016/07/03/python-coroutine/	Python协程：从yield/send到async/await
+	http://swig.org/papers/PyTutorial98/PyTutorial98.pdf	Interfacing C/C++ and Python with SWIG
