40b2f79acc8eb2a6d8f485bdf0b668d12f7533d1
xujie <jiexu@pptv.com>
2018-01-19 14:05:39 +0800

    add outer/tf/sample_tests/tf_sample_film.py

---
+++ outer/tf/sample_tests/tf_sample_film.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+import tensorflow as tf
+
+"""
+    是电影票房影响因素分析，现在有票房，评分，评分人数，影评类型，制片国家，还有CPI的数据 （Excel表格）
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
