17aab948888fb1c4b94355af75a61396eaf5729a
xujie <jiexu@pptv.com>
2018-03-07 16:21:00 +0800

    update wolf_nlp/算法学习笔记/NLP汉语自然语言处理原理与实践-读书笔记/20180214.md

---
+++ wolf_ml/tf/python_basic/python_basic4.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+
+def func1():
+    """
+    wsgi解析URL参数的时候:
+        解析 post请求中的参数：d = parse_qs(request_body)
+        解析 get请求的URL参数： d = parse_qs(environ['QUERY_STRING'])
+    """
+
+if __name__ == '__main__':
+    func1()
