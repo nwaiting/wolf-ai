0188378e55c5fc6b88af768b96428a7e2582c8e9
xujie <jiexu@pptv.com>
2018-03-08 16:25:34 +0800

    update wolf_nlp/算法学习笔记/NLP汉语自然语言处理原理与实践-读书笔记/20180307.md

---
+++ wolf_alg/Data_structures_and_algorithms_cpp/graph.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+/*
+  图存储方式有：邻接矩阵和邻接表
+*/
+
+template <typename T>
+struct ENode {
+    int32_t one_vertex_;
+    int32_t other_vertex_;
+    T weights_;
+    ENode<T> *next_;
+    ENode(){ next_ = NULL; }
+    ENode(int32_t onevertex, int32_t othervertex, T weights, )
+};
+
+int main(int argc, const char* argv[]) {
+    cout << "this is test" << endl;
+    Sleep(1000 * 100);
+    return 0;
+}
---
+++ wolf_alg/Data_structures_and_algorithms_cpp/readme.md
@@ -0,0 +1,2 @@
+参考 http://blog.csdn.net/ancientmoondjay/article/category/6783677 
+完成c++的常用的数据结构
