50acbd219481d97339a1ca5e05463e1849a0139a
xujie <jiexu@pptv.com>
2018-04-12 19:07:48 +0800

    update wolf_outer/wordcloud/wc.py

--- wolf_outer/wordcloud/wc.py
+++ wolf_outer/wordcloud/wc.py
@@ -1,6 +1,6 @@
 #coding=utf8
 
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 from matplotlib import pyplot as plt
 from scipy.misc import imread
 from snownlp import SnowNLP
@@ -10,16 +10,24 @@ import os
 生成词图
 """
 
-def buildWordCloud(file_data, file_pic):
+def buildWordCloud(file_data, file_pic, file_stop=None):
     with open(file_data, 'r', encoding='utf8') as fp:
         text = fp.read()
+        stopwords = None
+        if file_stop:
+            stopwords = STOPWORDS.copy()
+            with open(file_stop, 'r', encoding='utf8') as sfp:
+                for i in sfp.readlines():
+                    stopwords.add(i.strip('\r\n '))
         wc = WordCloud(background_color='white',
+                        stopwords=stopwords,
                         collocations=False,
                         width=1800,
                         height=1400,
                         font_path="C:\\Windows\\Fonts\\STFANGSO.ttf",
                         scale=2,
                         max_words=2000).generate(text=text)
+
         plt.imshow(wc)
         plt.axis('off')
         plt.show()
@@ -35,6 +43,7 @@ def emotionAnalysis(e_value_l, e_value_r, r_file, s_file):
             for line in fp.readlines():
                 s = SnowNLP(line)
                 if s.sentiments >= e_value_l and s.sentiments <= e_value_r:
+
                     rfp.write(' '.join(s.words))
 
 
@@ -48,10 +57,12 @@ if __name__ == '__main__':
     result_file_pic = os.path.join(file_path_parent, 'high.jpg')
     #原始数据文件
     source_file = os.path.join(file_path_parent, 'jd.allwords.data')
+    #屏蔽词
+    stop_words_file = os.path.join(file_path_parent, 'stop_words.data')
     if not os.path.exists(source_file):
         print('error !!!!!!!!!!! {0} not exists'.format(source_file))
-    emotionAnalysis(emotion_value_l, emotion_value_r, result_file_data, source_file)
-    buildWordCloud(result_file_data, result_file_pic)
+    #emotionAnalysis(emotion_value_l, emotion_value_r, result_file_data, source_file)
+    buildWordCloud(result_file_data, result_file_pic, stop_words_file)
 
 
 
