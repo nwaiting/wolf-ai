9463c099b5a4dece1894a38c3acf4549ddf6e859
xujie <jiexu@pptv.com>
2018-04-12 18:15:31 +0800

    update wolf_outer/wordcloud/wc.py

--- wolf_outer/wordcloud/wc.py
+++ wolf_outer/wordcloud/wc.py
@@ -3,27 +3,55 @@
 from wordcloud import WordCloud
 from matplotlib import pyplot as plt
 from scipy.misc import imread
+from snownlp import SnowNLP
 import os
 
-def main():
-    source_file = 'a.txt'
-    to_jpg_file = 'a.jpg'
-    to_jpg_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), to_jpg_file)
-    filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), source_file)
+"""
+生成词图
+"""
 
-    with open(filename, 'r', encoding='utf8') as fp:
+def buildWordCloud(file_data, file_pic):
+    with open(file_data, 'r', encoding='utf8') as fp:
         text = fp.read()
         wc = WordCloud(background_color='white',
+                        collocations=False,
+                        width=1800,
+                        height=1400,
                         font_path="C:\\Windows\\Fonts\\STFANGSO.ttf",
-                        scale=1.5).generate(text=text)
+                        scale=2,
+                        max_words=2000).generate(text=text)
         plt.imshow(wc)
         plt.axis('off')
         plt.show()
+        wc.to_file(file_pic)
 
-        wc.to_file(to_jpg_file)
+
+def emotionAnalysis(e_value_l, e_value_r, r_file, s_file):
+    """
+        根据情绪值范围过滤 句子
+    """
+    with open(r_file, 'w', encoding='utf8') as rfp:
+        with open(s_file, 'r', encoding='utf8') as fp:
+            for line in fp.readlines():
+                s = SnowNLP(line)
+                if s.sentiments >= e_value_l and s.sentiments <= e_value_r:
+                    rfp.write(' '.join(s.words))
+
 
 if __name__ == '__main__':
-    main()
+    #情绪值范围
+    emotion_value_l = 0.8
+    emotion_value_r = 1.0
+    file_path_parent = os.path.dirname(os.path.realpath(__file__))
+    #过滤后的情绪分词
+    result_file_data = os.path.join(file_path_parent, 'high.data')
+    result_file_pic = os.path.join(file_path_parent, 'high.jpg')
+    #原始数据文件
+    source_file = os.path.join(file_path_parent, 'jd.allwords.data')
+    if not os.path.exists(source_file):
+        print('error !!!!!!!!!!! {0} not exists'.format(source_file))
+    emotionAnalysis(emotion_value_l, emotion_value_r, result_file_data, source_file)
+    buildWordCloud(result_file_data, result_file_pic)
 
 
 
