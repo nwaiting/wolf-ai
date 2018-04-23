f7ed546f50b02054af27f46755b26841fb9bc81f
xujie <jiexu@pptv.com>
2018-04-23 12:46:09 +0800

    update wolf_outer/bayes_classifier

--- wolf_outer/bayes_classifier/NBayesClassifier.py
+++ wolf_outer/bayes_classifier/NBayesClassifier.py
@@ -1,55 +1,38 @@
-
 #coding: utf-8
 import os
 import time
 import random
 import snownlp
-import sklearn
 from sklearn.naive_bayes import MultinomialNB
-import matplotlib.pyplot as plt
 
 def MakeWordsSet(words_file):
     words_set = set()
-    with open(words_file, 'r') as fp:
+    with open(words_file, 'r', encoding='utf-8') as fp:
         for line in fp.readlines():
-            word = line.strip().decode("utf-8")
-            if len(word)>0 and word not in words_set: # 去重
+            word = line.strip('\r\n ')
+            if word and word not in words_set:
                 words_set.add(word)
     return words_set
 
-def TextProcessing(folder_path, test_size=0.2):
-    folder_list = os.listdir(folder_path)
+def TextProcessing(file_path, test_size=0.2):
     data_list = []
     class_list = []
+    with open(file_path, encoding='utf-8') as fp:
+        tag_flag = None
+        for line in fp.readlines():
+            line = line.strip('\r\n ')
+            if line:
+                if line.startswith('tag-'):
+                    tag_flag = line
+                else:
+                    if tag_flag:
+                        snow = snownlp.SnowNLP(line)
+                        word_list = snow.words
+                        data_list.append(word_list)
+                        class_list.append(tag_flag)
 
-    # 类间循环
-    for folder in folder_list:
-        new_folder_path = os.path.join(folder_path, folder)
-        files = os.listdir(new_folder_path)
-        # 类内循环
-        j = 1
-        for file in files:
-            if j > 100: # 每类text样本数最多100
-                break
-            with open(os.path.join(new_folder_path, file), 'r') as fp:
-               raw = fp.read()
-            # print raw
-            ## --------------------------------------------------------------------------------
-            ## jieba分词
-            # jieba.enable_parallel(4) # 开启并行分词模式，参数为并行进程数，不支持windows
-            #word_cut = jieba.cut(raw, cut_all=False) # 精确模式，返回的结构是一个可迭代的genertor
-            #word_list = list(word_cut) # genertor转化为list，每个词unicode格式
-            snow = snownlp.SnowNLP(raw)
-            word_list = snow.words()
-            # [我 来到 北京 清华大学]
-            ## --------------------------------------------------------------------------------
-            data_list.append(word_list)
-            class_list.append(folder.decode('utf-8'))
-            j += 1
-
-    ## 划分训练集和测试集
-    # train_data_list, test_data_list, train_class_list, test_class_list = sklearn.cross_validation.train_test_split(data_list, class_list, test_size=test_size)
-    data_class_list = zip(data_list, class_list)
+    # 随机取测试机和训练集
+    data_class_list = list(zip(data_list, class_list))
     random.shuffle(data_class_list)
     index = int(len(data_class_list)*test_size)+1
     train_list = data_class_list[index:]
@@ -57,29 +40,23 @@ def TextProcessing(folder_path, test_size=0.2):
     train_data_list, train_class_list = zip(*train_list)
     test_data_list, test_class_list = zip(*test_list)
 
-    # 统计词频放入all_words_dict
+    # 统计词频
     all_words_dict = {}
     for word_list in train_data_list:
         for word in word_list:
-            if all_words_dict.has_key(word):
-                all_words_dict[word] += 1
-            else:
-                all_words_dict[word] = 1
-    # key函数利用词频进行降序排序
-    all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f:f[1], reverse=True) # 内建函数sorted参数需为list
-    all_words_list = list(zip(*all_words_tuple_list)[0])
+            all_words_dict[word] = all_words_dict.get(word, 1) + 1
+
+    all_words_tuple_list = sorted(all_words_dict.items(), key=lambda f:f[1], reverse=True)
+    all_words_list = list(list(zip(*all_words_tuple_list))[0])
 
     return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list
 
-
 def words_dict(all_words_list, deleteN, stopwords_set=set()):
-    # 选取特征词
     feature_words = []
     n = 1
     for t in range(deleteN, len(all_words_list), 1):
         if n > 1000: # feature_words的维度1000
             break
-        # print all_words_list[t]
         if not all_words_list[t].isdigit() and all_words_list[t] not in stopwords_set and 1<len(all_words_list[t])<5:
             feature_words.append(all_words_list[t])
             n += 1
@@ -89,56 +66,53 @@ def words_dict(all_words_list, deleteN, stopwords_set=set()):
 def TextFeatures(train_data_list, test_data_list, feature_words):
     def text_features(text, feature_words):
         text_words = set(text)
-        ## -----------------------------------------------------------------------------------
-        ## sklearn特征 list
+        # sklearn特征 list
         features = [1 if word in text_words else 0 for word in feature_words]
-        ## -----------------------------------------------------------------------------------
         return features
     train_feature_list = [text_features(text, feature_words) for text in train_data_list]
     test_feature_list = [text_features(text, feature_words) for text in test_data_list]
     return train_feature_list, test_feature_list
 
 
-def TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list):
-    ## -----------------------------------------------------------------------------------
-    ## sklearn分类器
+def TextClassifier(train_feature_list, test_data_list, test_feature_list, train_class_list, test_class_list, deleteN=None, result_file=None):
+    # sklearn分类器
     classifier = MultinomialNB().fit(train_feature_list, train_class_list)
-    # print classifier.predict(test_feature_list)
-    # for test_feature in test_feature_list:
-    #     print classifier.predict(test_feature)[0],
-    # print ''
+    predicts = classifier.predict(test_feature_list)
+    if result_file:
+        with open(result_file, 'wb') as fp:
+            res = zip(test_data_list,test_class_list,test_feature_list,predicts)
+            for item in res:
+                fp.write(('test data： {0}\n'.format(item[0])).encode('utf-8'))
+                fp.write(('feature： {0}\n'.format(item[2])).encode('utf-8'))
+                fp.write(('test class： {0}\n'.format(item[1])).encode('utf-8'))
+                fp.write(('predict： {0}\n\n'.format(item[3])).encode('utf-8'))
     test_accuracy = classifier.score(test_feature_list, test_class_list)
     return test_accuracy
 
 
 if __name__ == '__main__':
-
-    print("start")
-
-    ## 文本预处理
-    folder_path = './Database/SogouC/Sample'
-    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(folder_path, test_size=0.2)
+    print('start bayes classifier')
+    # 读取数据
+    file_path = 'poems.txt'
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_path)
+    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list = TextProcessing(file_path, test_size=0.2)
 
     # 生成stopwords_set
-    stopwords_file = './stopwords_cn.txt'
-    stopwords_set = MakeWordsSet(stopwords_file)
+    stop_file_path = './stopwords.txt'
+    stop_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), stop_file_path)
+    stopwords_set = MakeWordsSet(stop_file_path)
 
-    ## 文本特征提取和分类
-    deleteNs = range(0, 1000, 20)
+    # 文本特征提取和分类
+    result_file = 'poems_results.txt'
+    result_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), result_file)
+    deleteNs = range(0, len(all_words_list), 10)
     test_accuracy_list = []
     for deleteN in deleteNs:
         feature_words = words_dict(all_words_list, deleteN, stopwords_set)
-        train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
-        test_accuracy = TextClassifier(train_feature_list, test_feature_list, train_class_list, test_class_list)
-        test_accuracy_list.append(test_accuracy)
+        if feature_words:
+            train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
+            test_accuracy = TextClassifier(train_feature_list, test_data_list, test_feature_list, train_class_list, test_class_list, deleteN=deleteN, result_file=result_file)
+            test_accuracy_list.append(test_accuracy)
     print(test_accuracy_list)
 
-    # 结果评价
-    plt.figure()
-    plt.plot(deleteNs, test_accuracy_list)
-    plt.title('Relationship of deleteNs and test_accuracy')
-    plt.xlabel('deleteNs')
-    plt.ylabel('test_accuracy')
-    plt.savefig('result.png')
-
-    print("finished")
+    print("end bayes classifier")
