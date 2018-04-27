73178b9af17c3773b281e62c25eaba0c680cafc0
xujie <jiexu@pptv.com>
2018-04-27 13:44:12 +0800

    update wolf_nlp/算法学习笔记/NLP汉语自然语言处理原理与实践-读书笔记/20180418-数学.md

--- wolf_ml/tf/python_basic/python_basic5.py
+++ wolf_ml/tf/python_basic/python_basic5.py
@@ -40,6 +40,48 @@ def func2():
     print(math.pow(2, 4))  #2^4
     print(math.sqrt(5))
 
+def func3():
+    punct = set(u''':!),.:;?]}¢'"、。〉》」』】〕〗〞︰︱︳﹐､﹒
+            ﹔﹕﹖﹗﹚﹜﹞！），．：；？｜｝︴︶︸︺︼︾﹀﹂﹄﹏､～￠
+            々‖•·ˇˉ―--′’”([{£¥'"‵〈《「『【〔〖（［｛￡￥〝︵︷︹︻
+            ︽︿﹁﹃﹙﹛﹝（｛“‘-—_…''')
+    # 对str/unicode
+    filterpunt = lambda s: ''.join(filter(lambda x: x not in punct, s))
+    # 对list
+    filterpuntl = lambda l: list(filter(lambda x: x not in punct, l))
+    st = '好,不.好!吧'
+    print(filterpunt(st)) #好不好吧
+
+    """
+    方法二：使用re
+    注：1、字符编码问题
+        2、pointwords中大小顺序问题  re.sub() 过滤使用的屏蔽字符从小到大
+    """
+    import re
+    pointwords = ['，', '、', '[', ']', '（', '）', '：',
+        '、', '。', '@', '’', '%', '《', '》', '“', '”', '.', '；',
+        '′', '°', '″', '-', ',', '！', '？','～', '\'', '\"', ':',
+        '(', ')', '【', '】', '~', '/', ';', '→', '\\', '·', '℃']
+    line = '好,不.好!吧'
+    line = re.sub(pointwords, '', line)
+    print(line)
+
+def func4():
+    from gensim.models.keyedvectors import KeyedVectors
+    from gensim.models.word2vec import Word2Vec
+    import os
+    bin_file = 'vectors.bin'
+    text_file = 'vectors.txt'
+    bin_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), bin_file)
+    text_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), text_file)
+    model = KeyedVectors.load_word2vec_format(bin_file, binary=True)
+    model.save_word2vec_format(text_file, binary=False)
+
+    model = Word2Vec()
+    model.wv.save_word2vec_format('a.txt', binary=False)
+
 if __name__ == '__main__':
     #func1()
-    func2()
+    #func2()
+    #func3()
+    func4()
--- wolf_outer/bayes_classifier/NBayesClassifier.py
+++ wolf_outer/bayes_classifier/NBayesClassifier.py
@@ -4,6 +4,8 @@ import time
 import random
 import snownlp
 from sklearn.naive_bayes import MultinomialNB
+from matplotlib import pyplot as plt
+from pylab import mpl
 
 def MakeWordsSet(words_file):
     words_set = set()
@@ -15,26 +17,29 @@ def MakeWordsSet(words_file):
     return words_set
 
 def TextPro(file_path):
-    test_class_list = []
+    test_data_list = []
     test_sentiments_list = []
     with open(file_path, encoding='utf-8') as fp:
         for line in fp.readlines():
             line = line.strip('\r\n ')
             find_index = line.find('?——?')
+            if find_index == -1:
+                find_index = line.find('——')
             if find_index != -1:
                 line = line[:find_index]
-                snow = snownlp.SnowNLP(line)
-                word_list = snow.words
-                data_list.append(word_list)
-                class_list.append(tag_flag)
-                sentiments_list.append(snow.sentiments)
-        return test_class_list, test_sentiments_list
+                try:
+                    snow = snownlp.SnowNLP(line)
+                    test_data_list.append(snow.words)
+                    test_sentiments_list.append(snow.sentiments)
+                except Exception as e:
+                    print('except {0} -- line {1}'.format(e, line))
+        return test_data_list, test_sentiments_list
 
 def TextProcessing(file_path, test_size=0.2):
     data_list = []
     class_list = []
     sentiments_list = []
-    with open(file_path, encoding='utf-8') as fp:
+    with open(file_path, 'r', encoding='utf-8') as fp:
         tag_flag = None
         for line in fp.readlines():
             line = line.strip('\r\n ')
@@ -44,6 +49,8 @@ def TextProcessing(file_path, test_size=0.2):
                 else:
                     if tag_flag:
                         find_index = line.find('?——?')
+                        if find_index == -1:
+                            find_index = line.find('——')
                         if find_index != -1:
                             line = line[:find_index]
                             snow = snownlp.SnowNLP(line)
@@ -72,7 +79,7 @@ def TextProcessing(file_path, test_size=0.2):
 
     return all_words_list, train_data_list, test_data_list, train_class_list, test_class_list, test_sentiments_list
 
-def words_dict(all_words_list, deleteN, stopwords_set=set()):
+def WordsDict(all_words_list, deleteN, stopwords_set=set()):
     feature_words = []
     n = 1
     for t in range(0, len(all_words_list), 1):
@@ -94,22 +101,64 @@ def TextFeatures(train_data_list, test_data_list, feature_words):
     test_feature_list = [text_features(text, feature_words) for text in test_data_list]
     return train_feature_list, test_feature_list
 
+def SplitSentiment(sent):
+    flag_sent = 0.0
+    if sent > 0.5:
+        if sent > 0.4 and sent <= 0.6:
+            flag_sent = 0.6
+        elif sent > 0.6 and sent <= 0.8:
+            flag_sent = 0.8
+        else:
+            flag_sent = 1.0
+    elif sent < 0.5:
+        if sent > 0.4 and sent <= 0.6:
+            flag_sent = 0.6
+        elif sent > 0.2 and sent <= 0.4:
+            flag_sent = 0.4
+        else:
+            flag_sent = 0.2
+    return flag_sent
+
 
 def TextClassifier(train_feature_list, test_data_list, test_feature_list, train_class_list, test_class_list, test_sentiments_list, result_file=None):
     # sklearn分类器
     classifier = MultinomialNB().fit(train_feature_list, train_class_list)
     predicts = classifier.predict(test_feature_list)
+    test_class_results = {}
+    test_sentiments_results = {}
     if result_file:
         with open(result_file, 'wb') as fp:
             res = zip(test_data_list,test_class_list,test_feature_list,predicts,test_sentiments_list)
             for item in res:
                 fp.write(('test data： {0}\n'.format(item[0])).encode('utf-8'))
-                fp.write(('feature： {0}\n'.format(item[2])).encode('utf-8'))
+                #fp.write(('feature： {0}\n'.format(item[2])).encode('utf-8'))
                 fp.write(('sentiment {0}\n'.format(item[4])).encode('utf-8'))
                 fp.write(('test class： {0}\n'.format(item[1])).encode('utf-8'))
                 fp.write(('predict： {0}\n\n'.format(item[3])).encode('utf-8'))
-    test_accuracy = classifier.score(test_feature_list, test_class_list)
-    return test_accuracy
+                test_class_results[item[3]] = test_class_results.get(item[3], 0) + 1
+                s_split = SplitSentiment(item[4])
+                test_sentiments_results[s_split] = test_sentiments_results.get(s_split, 0) + 1
+            for k,v in test_class_results.items():
+                fp.write(('{0} {1}'.format(k,v)).encode('utf-8'))
+
+        mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+        sd = sorted(test_sentiments_results.items(), key=lambda x:x[0])
+        plt.bar(range(len(sd)), [i[1] for i in sd], color='rgb',tick_label=[i[0] for i in sd])
+        plt.title(u'唐诗宋词情感分布图')
+        plt.savefig('{0}.{1}'.format(result_file, 'sentiments.jpg'))
+        plt.show()
+
+        print(test_class_results)
+        mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
+        tick_l = test_class_results.keys()
+        tick_l = [i.replace('tag-', '') for i in tick_l if i]
+        print(tick_l)
+        plt.bar(range(len(test_class_results)), test_class_results.values(), color='rgb', tick_label=tick_l)
+        plt.savefig('{0}.{1}'.format(result_file, 'classes.jpg'))
+        plt.title(u'唐诗宋词建筑分类分布图')
+        plt.show()
+    #test_accuracy = classifier.score(test_feature_list, test_class_list)
+    #return test_accuracy
 
 
 if __name__ == '__main__':
@@ -117,17 +166,17 @@ if __name__ == '__main__':
     # 读取数据
     file_path = 'poems.txt'
     file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_path)
-    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list, test_sentiments_list = TextProcessing(file_path, test_size=0.2)
+    all_words_list, train_data_list, test_data_list, train_class_list, test_class_list, test_sentiments_list = TextProcessing(file_path, test_size=0.1)
 
     #测试数据
     file_path = 'poems_test.txt'
     file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_path)
     if os.path.exists(file_path):
-        t_test_class_list, t_test_sentiments_list = TextPro(file_path)
-        if len(t_test_class_list) > 0 and len(t_test_sentiments_list) > 0:
-            test_class_list = t_test_class_list[:]
+        t_test_data_list, t_test_sentiments_list = TextPro(file_path)
+        if len(t_test_data_list) > 0 and len(t_test_sentiments_list) > 0:
+            test_data_list = t_test_data_list[:]
+            test_class_list = [None for _ in range(len(t_test_data_list))]
             test_sentiments_list = t_test_sentiments_list[:]
-            train_class_list = [None for _ in range(len(test_class_list))]
 
     # 生成stopwords_set
     stop_file_path = 'stopwords.txt'
@@ -140,7 +189,7 @@ if __name__ == '__main__':
     deleteNs = range(0, len(all_words_list), 20)
     test_accuracy_list = []
     for deleteN in deleteNs:
-        feature_words = words_dict(all_words_list, deleteN, stopwords_set)
+        feature_words = WordsDict(all_words_list, deleteN, stopwords_set)
         if feature_words:
             train_feature_list, test_feature_list = TextFeatures(train_data_list, test_data_list, feature_words)
             test_accuracy = TextClassifier(train_feature_list, test_data_list, test_feature_list, train_class_list, test_class_list, test_sentiments_list, result_file=result_file)
