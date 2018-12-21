dda42806965d7f9e46faae33a3cd87698323b89e
xujie <jiexu@pptv.com>
2018-12-21 10:06:01 +0800

    wolf_nlp/算法学习笔记

---
+++ wolf_nlp/算法学习笔记/NLP-常用技术学习/20181220-cfg-cyk.py
@@ -0,0 +1,17 @@
+#coding=utf8
+
+"""
+    CYK（Cocke-Younger-Kasami）算法：
+        概述：
+            在计算机领域，CYK算法是一种用来对上下文无关文法（CFG）进行语法解析的算法。
+            CYK算法是一种基于动态规划思想设计的一种自底向上语法分析算法
+
+        应用：
+            CFG和Parsing在编译技术和自然语言处理中都有用到
+"""
+
+def main():
+    print("test")
+
+if __name__ == '__main__':
+    main()
