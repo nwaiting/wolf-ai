ffdfa1581ec5f1677b77cd9e9e9b834d2cb89aca
xujie <jiexu@pptv.com>
2018-12-28 20:52:18 +0800

    update wolf_alg/Data_structures_and_algorithms_cpp

---
+++ wolf_alg/Data_structures_and_algorithms_cpp/20181228-dp.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <vector>
+#include <stdint.h>
+#include <algorithm>
+using namespace std;
+
+/*
+*   编辑距离：
+*       编辑距离又叫Levenshtein（莱文斯坦）距离
+*       指两个字串之间，由一个转成另一个所需的最少编辑操作次数
+*       俄罗斯科学家Vladimir Levenshtein在1965年提出这个概念
+*
+*   编辑操作：
+*       1、替换一个字符
+*       2、插入一个字符
+*       3、删除一个字符
+*
+*/
+
+int edit_dis(const std::string &s1, const std::string &s2)
+{
+    std::vector<std::vector<int32_t>> editdis;
+    std::vector<int32_t> initdata(s2.size()+1, 0);
+    for (auto i = 0; i <= s1.size(); i++) {
+        editdis.push_back(initdata);
+    }
+
+    //先进行初始化
+    for (auto i = 0; i <= s1.size(); i++) {
+        editdis[i][0] = i;
+    }
+
+    for (auto i = 0; i <= s2.size(); i++) {
+        editdis[0][i] = i;
+    }
+
+    //增、删、改三步
+    for (auto i = 1; i <= s1.size(); i++) {
+        for (auto j = 1; j <= s2.size(); j++) {
+            //增和删
+            int32_t tmp_min = (std::min)(editdis[i - 1][j] + 1, editdis[i][j - 1] + 1);
+            int32_t d = 0;
+            if (s1[i-1] != s2[j-1]) {
+                d = 1;
+            }
+            //改
+            tmp_min = (std::min)(tmp_min, editdis[i - 1][j - 1] + d);
+            editdis[i][j] = tmp_min;
+        }
+    }
+
+#ifdef DEBUG
+    for (auto &iti : editdis) {
+        for (auto &itj : iti) {
+            std::cout << itj << " ";
+        }
+        std::cout << std::endl;
+    }
+#endif
+
+    return editdis[s1.size()][s2.size()];
+}
+
+void showDistance(const std::string &s1, const std::string &s2)
+{
+    std::cout << s1.c_str() << " - " << s2.c_str() << " -> " << edit_dis(s1, s2) << std::endl;
+}
+
+int main()
+{
+    showDistance("sailn", "failing");   //1
+    //由于每个汉字两个字符 所以结果为2
+    showDistance("汽车音响改装线路","汽车音响改装线路图");   //2
+
+
+    std::cin.get();
+    return 0;
+}
---
+++ wolf_alg/Data_structures_and_algorithms_cpp/20181228-dp2.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <stdint.h>
+#include <list>
+using namespace std;
+
+/*
+*   LCS(最长公共子序列)、LIS（最长递增子序列）、最大子序列、最长公共子串（要求连续）
+*
+*
+*
+*
+*/
+
+/////////////////////////////// LCS 最长公共子序列
+
+int LCS(const std::string &s1, const std::string &s2)
+{
+    std::vector<std::vector<int32_t>> commonstr;
+    std::vector<int32_t> tmp_vec(s2.size()+1, 0);
+    for (auto i = 0; i <= s1.size(); i++) {
+        commonstr.push_back(tmp_vec);
+    }
+
+    //dp
+    for (auto i = 1; i <= s1.size(); i++) {
+        for (auto j = 1; j <= s2.size(); j++) {
+            //当判断是中文时，需要改一下
+            if (s1[i-1] == s2[j-1]) {
+                commonstr[i][j] = commonstr[i-1][j-1] + 1;
+            }
+            else {
+                commonstr[i][j] = (std::max)(commonstr[i - 1][j], commonstr[i][j - 1]);
+            }
+        }
+    }
+
+#ifdef DEBUG
+    for (auto &iti:commonstr) {
+        for (auto &itj: iti) {
+            std::cout << itj << " ";
+        }
+        std::cout << std::endl;
+    }
+#endif
+
+    std::list<int8_t> commlist;
+    int32_t ii = s1.size(), jj = s2.size();
+    //commlist.push_back(commonstr[ii][jj]);
+    commlist.push_back(s1[ii]);
+    while (ii > 0 && jj > 0) {
+        if (commonstr[ii-1][jj] == commonstr[ii][jj]) {
+            ii--;
+        }
+        else {
+            //commlist.push_back(commonstr[ii-1][jj-1]);
+            commlist.push_back(s1[ii-1]);
+            ii--;
+            jj--;
+        }
+    }
+
+
+    while (!commlist.empty()) {
+        printf("%c ", commlist.back());
+        commlist.pop_back();
+    }
+    std::cout << std::endl;
+
+    return commonstr[s1.size()][s2.size()];
+}
+
+void showLCS(const std::string &s1, const std::string &s2)
+{
+    std::cout<<"LCS: " << s1.c_str()<<" - "<<s2.c_str()<<" -> " << LCS(s1, s2) << std::endl;
+}
+
+
+///////////////////最长功能字串
+
+int32_t LCSubstring(const std::string &s1, const std::string &s2)
+{
+    int32_t longest_lcsubstring = 0;
+    std::vector<std::vector<int32_t>> dp;
+    std::vector<int32_t> tmp_vec(s2.size() + 1, 0);
+    for (auto i = 0; i <= s1.size(); i++) {
+        dp.push_back(tmp_vec);
+    }
+
+    for (auto i = 1; i <= s1.size(); i++) {
+        for (auto j = 1; j <= s2.size(); j++) {
+            if (s1[i] == s2[j]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                longest_lcsubstring = (std::max)(longest_lcsubstring, dp[i][j]);
+            }
+        }
+    }
+
+#ifdef DEBUG
+    for (auto &iti:dp) {
+        for (auto &itj:iti) {
+            std::cout << itj << " ";
+        }
+        std::cout << std::endl;
+    }
+#endif
+
+    std::list<int8_t> commonlist;
+    bool flag = true;
+    for (auto i = s1.size(); i > 0 && flag; i--) {
+        for (auto j = s2.size(); j > 0 && flag; j--) {
+            if (dp[i][j] == longest_lcsubstring) {
+                int32_t tmp_l = longest_lcsubstring;
+                while (tmp_l > 0) {
+                    commonlist.push_back(s1[i]);
+                    tmp_l--;
+                    i--;
+                }
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    while (!commonlist.empty()) {
+        printf("%c ", commonlist.back());
+        commonlist.pop_back();
+    }
+    std::cout << std::endl;
+
+    return longest_lcsubstring;
+}
+
+void showLCSubstring(const std::string &s1, const std::string &s2)
+{
+    std::cout <<"lcsubstring: "<< s1.c_str() << " - " << s2.c_str() << " -> " << LCSubstring(s1, s2) << std::endl;
+}
+
+
+//////////////////////LIS
+
+int32_t LIS(const std::string &s1, const std::string &s2)
+{
+
+}
+
+void showLIS(const std::string &s1, const std::string &s2)
+{
+    std::cout << "LIS:" << s1.c_str() << " - " << s2.c_str() << " -> " << LIS(s1, s2) << std::endl;
+}
+
+int main()
+{
+    showLCS("ABCBDAB", "BDCABA");
+    showLCS("成都空调移机多少钱", "成都空调移机多少钱");
+    showLCS("房地产中介好做吗", "苹果6现在多少钱");
+
+    showLCSubstring("ABCABABD", "BDCABA");
+
+    std::cin.get();
+    return 0;
+}
