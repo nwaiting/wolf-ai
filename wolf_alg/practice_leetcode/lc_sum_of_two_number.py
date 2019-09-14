4ffb0a23f9545cdf667de460b4d93069f2fb0182
abcd1f2 <798990255@qq.com>
2019-09-15 00:54:52 +0800

    update wolf_alg

--- wolf_alg/practice_leetcode/lc_sum_of_two_number.py
+++ wolf_alg/practice_leetcode/lc_sum_of_two_number.py
@@ -52,6 +52,24 @@ def addTwoNumbers(l1, l2):
         result_node.next_ = node
     return result_node.next_
 
+def addTwoNumbers2(l1,l2):
+    res_node = Node()
+    carry = 0
+    p1 = res_node
+    while carry or l1 or l2:
+        if l1:
+            carry += l1.data_
+            l1 = l1.next_
+        if l2:
+            carry += l2.data_
+            l2 = l2.next_
+        node = Node(carry%10)
+        carry = carry//10
+
+        p1.next_ = node
+        p1 = node
+    return res_node.next_
+
 if __name__ == '__main__':
     nums = [1,3,6,8,12,15,17,20,123,2,34,345,36,2342,3345]
     begin1 = time.time()
@@ -62,11 +80,17 @@ if __name__ == '__main__':
     print("first={},second={}".format(begin2-begin1, end-begin2))
 
 
-    fnode = Node(4, Node(5, Node(6)))
+    fnode = Node(4, Node(5, Node(6, Node(7))))
     snode = Node(7, Node(8, Node(9)))
     res = addTwoNumbers(fnode, snode)
-    print("654+987=",end='')
+    print("7654+987=",end='')
     while res:
         print(res.data_, end='')
         res=res.next_
+    print()
+    res2 = addTwoNumbers2(fnode,snode)
+    print("456+7899=",end='')
+    while res2:
+        print(res2.data_, end='')
+        res2=res2.next_
     print()
