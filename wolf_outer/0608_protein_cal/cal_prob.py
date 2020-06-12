42337b44b1fa08e78aac2d05715aa352857c4dd7
xj <xj@qq.com>
2020-06-12 13:54:37 +0800

    update wolf_outer

--- wolf_outer/0608_protein_cal/cal_prob.py
+++ wolf_outer/0608_protein_cal/cal_prob.py
@@ -11,16 +11,20 @@ def main():
     """
         python cal_prob.py data 1 result.txt
     """
-    def save_prob_to_file():
+    total_index = 0
+
+    def save_prob_to_file(tag_flag, pro_name):
         tags_line = ''
         prob_line = ''
         for k,v in search_str_probs.items():
             tags_line += "{}\t".format(k)
             prob_line += "{}\t".format(v)
         save_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), result_file)
-        with open(save_file, 'wb') as f:
-            f.write(("{}\n".format(tags_line)).encode('utf-8'))
-            f.write(("{}\n".format(prob_line)).encode('utf-8'))
+        with open(save_file, 'ab') as f:
+            if tag_flag <= 0:
+                f.write(("{}\n".format(tags_line)).encode('utf-8'))
+                tag_flag += 1
+            f.write((">{}\t{}\n".format(pro_name, prob_line)).encode('utf-8'))
     if len(sys.argv) != 4:
         print("args missing")
         return
@@ -43,56 +47,62 @@ def main():
                 if protein_name not in protein_info:
                     protein_info[protein_name] = []
                 protein_info[protein_name].append(line)
-    search_str = ''.join(protein_info[protein_name])
-    search_str_probs = {}
-    signal_char_str = 'ACDEFGHIKLMNPQRSTVWY'
-    if ana_type == 1:
-        # 计算单个频率
-        total_len = len(search_str)
-        for c in signal_char_str:
-            res = re.findall(c, search_str)
-            if res:
-                search_str_probs[c] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
-            else:
-                search_str_probs[c] = 0
-        save_prob_to_file()
-    elif ana_type == 2:
-        # 二联体
-        total_len = len(search_str)*(len(search_str)+1)*(2*len(search_str)+1)/6
-        for cc in permutations(signal_char_str, 2):
-            cc = ''.join(cc)
-            res = re.findall(cc, search_str)
-            if res:
-                search_str_probs[cc] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
-            else:
-                search_str_probs[cc] = 0
-        for cc in signal_char_str:
-            cc = cc*2
-            res = re.findall(cc, search_str)
-            if res:
-                search_str_probs[cc] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
-            else:
-                search_str_probs[cc] = 0
-        save_prob_to_file()
-    elif ana_type == 3:
-        # 三联体
-        total_len = (len(search_str)*(len(search_str)+1)/2)*(len(search_str)*(len(search_str)+1)/2)
-        items = ['AGV','ILFP','YMTS','HNQW','RK','DE','C']
-        new_items = [''.join(cc) for cc in permutations(items)] + [cc*2 for cc in items]
-        last_items = []
-        for one in new_items:
-            for two in items:
-                last_items.append(one+two)
-        for item in last_items:
-            res = re.findall(item, search_str)
-            if res:
-                search_str_probs[item] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
-            else:
-                search_str_probs[item] = 0
-        save_prob_to_file()
-    else:
-        print("args err {}".format(sys.argv))
+
+    for pk,pv in protein_info.items():
+        search_str = ''.join(pv)
+        search_str_probs = {}
+        signal_char_str = 'ACDEFGHIKLMNPQRSTVWY'
+        if ana_type == 1:
+            # 计算单个频率
+            total_len = len(search_str)
+            for c in signal_char_str:
+                res = re.findall(c, search_str)
+                if res:
+                    search_str_probs[c] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
+                else:
+                    search_str_probs[c] = 0
+            save_prob_to_file(total_index, pk)
+        elif ana_type == 2:
+            # 二联体
+            total_len = len(search_str)*(len(search_str)+1)*(2*len(search_str)+1)/6
+            for cc in permutations(signal_char_str, 2):
+                cc = ''.join(cc)
+                res = re.findall(cc, search_str)
+                if res:
+                    search_str_probs[cc] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
+                else:
+                    search_str_probs[cc] = 0
+            for cc in signal_char_str:
+                cc = cc*2
+                res = re.findall(cc, search_str)
+                if res:
+                    search_str_probs[cc] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
+                else:
+                    search_str_probs[cc] = 0
+            save_prob_to_file(total_index, pk)
+        elif ana_type == 3:
+            # 三联体
+            total_len = (len(search_str)*(len(search_str)+1)/2)*(len(search_str)*(len(search_str)+1)/2)
+            items = ['AGV','ILFP','YMTS','HNQW','RK','DE','C']
+            new_items = [''.join(cc) for cc in permutations(items, 2)] + [cc*2 for cc in items]
+            last_items = []
+            for one in new_items:
+                for two in items:
+                    last_items.append(one+two)
+            for item in last_items:
+                res = re.findall(item, search_str)
+                if res:
+                    search_str_probs[item] = format(len(res)/total_len, '.{}f'.format(last_flost_bit))
+                else:
+                    search_str_probs[item] = 0
+            save_prob_to_file(total_index, pk)
+        else:
+            print("args err {}".format(sys.argv))
+        total_index += 1
 
 
 if __name__ == '__main__':
     main()
+
+
+
--- wolf_outer/0608_protein_cal/cal_split.py
+++ wolf_outer/0608_protein_cal/cal_split.py
@@ -7,7 +7,7 @@ def main():
         python cal_split.py data 1
     """
     def save_split_data(filename, data, protein_name):
-        with open(filename, 'wb') as f:
+        with open(filename, 'ab') as f:
             f.write((">{}\n".format(protein_name)).encode('utf-8'))
             f.write(("{}\n".format(data)).encode('utf-8'))
     if len(sys.argv) != 3:
@@ -31,65 +31,66 @@ def main():
                 if protein_name not in protein_info:
                     protein_info[protein_name] = []
                 protein_info[protein_name].append(line)
-    search_str = ''.join(protein_info[protein_name])
-    search_str_len = len(search_str)
-    start1 = int(search_str_len * 0.618)
-    show_protein_str = ''
-    if ana_type == 1:
-        save_split_data("{}_1.txt".format(ana_type), search_str[:start1], protein_name)
-        save_split_data("{}_2.txt".format(ana_type), search_str[start1:], protein_name)
-        show_protein_str = search_str[start1:]
-    if ana_type == 2:
-        new_split_list = []
-        for item in (search_str[:start1], search_str[start1:]):
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_split_list.append(item[:start1_1])
-            new_split_list.append(item[start1_1:])
-            show_protein_str = item[start1_1:]
-        for i in range(len(new_split_list)):
-            save_split_data("{}_{}.txt".format(ana_type, i+1), new_split_list[i], protein_name)
-    if ana_type == 3:
-        new_split_list = []
-        new_new_split_list = []
-        for item in (search_str[:start1], search_str[start1:]):
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_split_list.append(item[:start1_1])
-            new_split_list.append(item[start1_1:])
-        for item in new_split_list:
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_new_split_list.append(item[:start1_1])
-            new_new_split_list.append(item[start1_1:])
-            show_protein_str = item[start1_1:]
-        for i in range(len(new_new_split_list)):
-            save_split_data("{}_{}.txt".format(ana_type, i+1), new_new_split_list[i], protein_name)
-    if ana_type == 4:
-        new_split_list = []
-        new_new_split_list = []
-        new_new_new_split_list = []
-        for item in (search_str[:start1], search_str[start1:]):
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_split_list.append(item[:start1_1])
-            new_split_list.append(item[start1_1:])
-        for item in new_split_list:
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_new_split_list.append(item[:start1_1])
-            new_new_split_list.append(item[start1_1:])
-        for item in new_new_split_list:
-            search_str_len = len(item)
-            start1_1 = int(search_str_len * 0.618)
-            new_new_new_split_list.append(item[:start1_1])
-            new_new_new_split_list.append(item[start1_1:])
-            show_protein_str = item[start1_1:]
-        for i in range(len(new_new_new_split_list)):
-            save_split_data("{}_{}.txt".format(ana_type, i+1), new_new_new_split_list[i], protein_name)
-    print("最大分割次数：{}".format(ana_type))
-    print("序列名字：>{}".format(protein_name))
-    print("最短序列：{}".format(show_protein_str))
+    for pk,pv in protein_info.items():
+        search_str = ''.join(pv)
+        search_str_len = len(search_str)
+        start1 = int(search_str_len * 0.618)
+        show_protein_str = ''
+        if ana_type == 1:
+            save_split_data("{}_1.txt".format(ana_type), search_str[:start1], pk)
+            save_split_data("{}_2.txt".format(ana_type), search_str[start1:], pk)
+            show_protein_str = search_str[start1:]
+        if ana_type == 2:
+            new_split_list = []
+            for item in (search_str[:start1], search_str[start1:]):
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_split_list.append(item[:start1_1])
+                new_split_list.append(item[start1_1:])
+                show_protein_str = item[start1_1:]
+            for i in range(len(new_split_list)):
+                save_split_data("{}_{}.txt".format(ana_type, i+1), new_split_list[i], pk)
+        if ana_type == 3:
+            new_split_list = []
+            new_new_split_list = []
+            for item in (search_str[:start1], search_str[start1:]):
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_split_list.append(item[:start1_1])
+                new_split_list.append(item[start1_1:])
+            for item in new_split_list:
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_new_split_list.append(item[:start1_1])
+                new_new_split_list.append(item[start1_1:])
+                show_protein_str = item[start1_1:]
+            for i in range(len(new_new_split_list)):
+                save_split_data("{}_{}.txt".format(ana_type, i+1), new_new_split_list[i], pk)
+        if ana_type == 4:
+            new_split_list = []
+            new_new_split_list = []
+            new_new_new_split_list = []
+            for item in (search_str[:start1], search_str[start1:]):
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_split_list.append(item[:start1_1])
+                new_split_list.append(item[start1_1:])
+            for item in new_split_list:
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_new_split_list.append(item[:start1_1])
+                new_new_split_list.append(item[start1_1:])
+            for item in new_new_split_list:
+                search_str_len = len(item)
+                start1_1 = int(search_str_len * 0.618)
+                new_new_new_split_list.append(item[:start1_1])
+                new_new_new_split_list.append(item[start1_1:])
+                show_protein_str = item[start1_1:]
+            for i in range(len(new_new_new_split_list)):
+                save_split_data("{}_{}.txt".format(ana_type, i+1), new_new_new_split_list[i], pk)
+        print("最大分割次数：{}".format(ana_type))
+        print("序列名字：>{}".format(protein_name))
+        print("最短序列：{}".format(show_protein_str))
 
 
 if __name__ == '__main__':
