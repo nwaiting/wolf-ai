f9fec841b6cabfbda2bed80671200dbcddcb619d
xj <xj@qq.com>
2020-06-16 12:37:50 +0800

    update wolf_outer

--- wolf_outer/0616_block_coin/block_coin.py
+++ wolf_outer/0616_block_coin/block_coin.py
@@ -1,49 +1,53 @@
-import hashlib  # 加密模块
-import datetime  # 时间模块
+import hashlib
+import datetime
 
 
-class DaDaBlockCoin:  # 电子货币，达达币
-    def __init__(self, index,  # 索引
-                 timestamp,  # 交易时间
-                 data,  # 交易记录
-                 next_hash):  # 下个哈希
+class EncryBlock:
+    def __init__(self, index, timestamp, data, next_hash):
+        # 索引
         self.index = index
+        # 时间
         self.timestamp = timestamp
+        # 记录
         self.data = data
-        self.next_hash = next_hash  # 下一个
-        self.selfhash = self.hash_DaDaBlockCoin()  # 自身哈希
+        # 下个哈希
+        self.next_hash = next_hash
+        # 自己哈希
+        self.self_hash = self.hash_self()
 
-    def hash_DaDaBlockCoin(self):
-        sha = hashlib.sha512()  # 加密算法
-        datastr = str(self.index) + str(self.timestamp) + str(self.data) + str(self.next_hash)  # 对于数据整体加密
-        sha.update(datastr.encode("utf-8"))  # 二进制
+    def hash_self(self):
+        sha = hashlib.sha512()
+        datastr = str(self.index) + str(self.timestamp) + str(self.data) + str(self.next_hash)
+        sha.update(datastr.encode("utf-8"))
         return sha.hexdigest()
 
 
-def create_first_DadaBlock():  # 创世区块链diyikuai
-    return DaDaBlockCoin(0, datetime.datetime.now(), "Love Dada", "0", )
+def create_first_block():
+    return EncryBlock(0, datetime.datetime.now(), "Love", "0")
 
 
-def create_money_DadaBlock(last_block):  # 区块链的其他块
+# 其他块
+def create_money_block(last_block):
     this_index = last_block.index + 1  # 索引加1
     this_timestamp = datetime.datetime.now()  # 当前时间
-    this_data = "love Dada" + str(this_index)  # 取得上一块的哈希
-    this_hash = last_block.selfhash  # 取得上一块的哈希
-    return DaDaBlockCoin(this_index,
-                         this_timestamp,
-                         this_data,
-                         this_hash)
+    this_data = "Love" + str(this_index)  # 取得上一块的哈希
+    this_hash = last_block.self_hash  # 取得上一块的哈希
+    return EncryBlock(this_index, this_timestamp, this_data, this_hash)
 
 
-DaDaBlockCoins = [create_first_DadaBlock()]  # 区块链列表，只有一个创世模块
+# 列表只有一个创世模块
+block_coins = [create_first_block()]
 nums = 100
-head_block = DaDaBlockCoins[0]  # 一开始等于第一个
-print(head_block.index, head_block.timestamp, head_block.selfhash, head_block.next_hash)
+# 开始只有一个
+head_block = block_coins[0]
+print("start ",head_block.index, head_block.timestamp, head_block.self_hash, head_block.next_hash)
 for i in range(nums):
-    dadaBlock_add = create_money_DadaBlock(head_block)
-    DaDaBlockCoins.append(dadaBlock_add)  # 加入区块链
-    head_block = dadaBlock_add  # 循环
-    print(dadaBlock_add.index, dadaBlock_add.timestamp, dadaBlock_add.selfhash, dadaBlock_add.next_hash)
+    block_item = create_money_block(head_block)
+    # 加入区块链
+    block_coins.append(block_item)
+    # 循环
+    head_block = block_item
+    print(block_item.index, block_item.timestamp, block_item.self_hash, block_item.next_hash)
 
 
 
