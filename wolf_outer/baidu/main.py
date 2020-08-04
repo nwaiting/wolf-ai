5b75c04ff410b80539ed87e6b1ca8563334cb446
abcd1f2 <798990255@qq.com>
2020-08-04 23:50:34 +0800

    update wolf_outer

--- wolf_outer/baidu/main.py
+++ wolf_outer/baidu/main.py
@@ -3,21 +3,48 @@ import time
 import random
 from lxml import etree
 import re
+import openpyxl
 
 
 str_set = {'辞职',
            '离开',
-           '结束'}
+           '结束',
+           '不再担任',
+           '卸任',
+           '出任'}
+
+search_pattern_list = [
+    '离职',
+    '出任'
+]
+
+f = open("{}_{}.txt".format('results', time.time()), 'wb')
+
+def read_excel(excel_file_name):
+    wb = openpyxl.load_workbook(excel_file_name)
+
+    ws = wb.get_sheet_by_name(wb.sheetnames[0])
+    lines = []
+    for i in range(2, ws.max_row+1):
+        line = ''
+        for j in range(2, ws.max_column+1):
+            line += "{} ".format(ws.cell(i,j).value)
+        lines.append(line)
+
+    wb.close()
+    return lines
 
 
-def main(datas):
+def main(word, time_pre):
     url = 'http://www.baidu.com/s'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36",
         "X-Requested-With": "XMLHttpRequest"
     }
 
-    for word in datas:
+    print("{} {} ===================== ".format(word, time_pre))
+    f.write("{} {} ===================== \n".format(word, time_pre).encode('utf-8'))
+    for p in search_pattern_list:
         params = {
             "ie": "utf-8",
             "cl": "3",
@@ -28,8 +55,7 @@ def main(datas):
             "rsv_bp": "1",
             "rsv_idx": "2",
             "tn": "baiduhome_pg",
-            "wd": "{}".format(word),
-            "bs": "{}".format(word),
+            "wd": "{} {}".format(word, p),
             "rsv_spt": "1",
             "rsv_pq": "c666451600029c2c",
             "rqlang": "cn",
@@ -53,6 +79,7 @@ def main(datas):
                 for set_item in str_set:
                     if re.findall(set_item, item_str):
                         print(item_str)
+                        f.write("{}\n".format(item_str).encode('utf-8'))
         except Exception as e:
             print('err {}'.format(e))
         else:
@@ -62,9 +89,13 @@ def main(datas):
 
 
 if __name__ == '__main__':
-    words = ['万科A 王石', '沙河股份 杨建达']
-    main(words)
+    datas = read_excel('ggg.xlsx')
+    for data in datas[:2]:
+        data_items = data.split()
+        if len(data_items) == 3:
+            time_items = data_items[0].split('-')
+            main("{} {}".format(data_items[1], data_items[2]), time_items[0])
 
-
+    f.close()
 
 
