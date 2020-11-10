88e956ba9b6cd8394b4ef350800c5b4b739390b9
xujie <xujie@qq.com>
2020-11-10 18:36:32 +0800

    update wolf_outer

---
+++ wolf_outer/spider_bases/bus.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS `tb_goods` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `uuid` VARCHAR(40) NOT NULL,
+    `brandId` VARCHAR(64) NULL DEFAULT NULL COMMENT '频道id',
+    `productId` VARCHAR(64) NULL DEFAULT NULL COMMENT '产品id',
+    `good_id` VARCHAR(64) NULL DEFAULT NULL COMMENT '物品id(详情中的)',
+    `title` VARCHAR(64) NULL DEFAULT NULL COMMENT '标题',
+    `pic` VARCHAR(256) NULL DEFAULT NULL COMMENT '图片url',
+    `detail` VARCHAR(256) NULL DEFAULT NULL COMMENT '链接',
+    `saleDiscount` VARCHAR(40) NULL DEFAULT NULL COMMENT 'discount',
+    `discount` float NULL DEFAULT NULL COMMENT 'discount',
+    `price` float NULL DEFAULT NULL COMMENT 'price',
+    `marketPrice` float NULL DEFAULT NULL COMMENT 'marketPrice',
+    `source_extern` VARCHAR(256) NULL DEFAULT NULL COMMENT 'source的其他',
+    `source` VARCHAR(64) NULL DEFAULT NULL COMMENT '来源(如vip)',
+    `du_price` float NULL DEFAULT NULL COMMENT 'du price',
+    `du_count` INT(11) NULL DEFAULT NULL COMMENT 'du count',
+    `extern` JSON NULL DEFAULT NULL COMMENT 'extern',
+    `updated_ts` BIGINT(20) NULL DEFAULT NULL,
+    `updated_date` DATETIME NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `tb_goods_uuid_IDX` (`uuid`) USING BTREE,
+    INDEX `tb_goods_discount_IDX` (`discount`) USING BTREE,
+    INDEX `tb_goods_updated_ts_IDX` (`updated_ts`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci' ENGINE=InnoDB;
+
